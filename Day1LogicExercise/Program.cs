79f85f9a1c31f0268e42d9c53c07d34236273a66
Sarah Gustin <sarahngustine02@gmail.com>
2026-03-31 14:40:17 +0700

    day 1 logic exercise

---
+++ Day1LogicExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BootcampSE
+{
+ class Program
+   {
+       static void Main(string[] args)
+       {
+        Console.WriteLine("Masukkan Nilai : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int x = 1; x <= n; x++)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.Write("foobar");
+
+            }
+            else if (x % 3 == 0)
+            {
+                Console.Write ("foo, ");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.Write ("bar, ");
+            }
+            else
+            {
+                Console.Write(x + ", ");
+            }
+        }
+       }
+   }
+}
